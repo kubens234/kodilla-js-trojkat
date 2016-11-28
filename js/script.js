af28f421a434d29e982a371cea9913057cf15147
jakub <mroczkowski.jakub84@gmail.com>
2016-11-28 15:31:06 +0100

    instrukcje warunkowe w praktyce

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-var a = prompt('Podaj podstawę trójkąta');
-if (a>0) {
-var h = prompt('Podaj wysokość trójkąta');
-if (h>0) { 
-var	triangleArea = a*h/2;
+var a = prompt('Podaj pierwszą wartość'),
+	b = prompt('Podaj drugą wartość'),
+	value = (a*a) + (2 * a * b) - (b * b);
 
-document.write('Podałeś podstwę trójkąta: ' + a);
+console.log(value);
+document.write('Podałeś pierwszą wartość: ' + a);
 document.write('<br >');
-document.write('Podałeś wysokość trójkąta: ' + h);
+document.write('Podałeś drugą wartość: ' + b);
 document.write('<br >');
-document.write('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 
-
-
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-}
-else {
-	document.write('Podałeś podstwę trójkąta: ' + a);
-	document.write('<br >');
-	document.write('Wysokość musi być liczbą dodatnią');
-	console.log('Podałeś: ' + h +'Podana liczba musi być dodatnia!!');
-}
+if (value > 0) {
+	console.log('Wartość value jest większa od 0');
+	document.write('Wynik działania (a*a) + (2 * a * b) - (b * b) jest większy od zera i wynosi: ' + value);
 } else {
-	document.write('podaj liczbę dodatnią podstawy');
-	console.log('Podałeś: ' + a +'Podana liczba musi być dodatnia!!');
+	console.log('Wartość value jest mniejsza od 0');
+	document.write('Wynik działania (a*a) + (2 * a * b) - (b * b) jest mniejszy od zera i wynosi: ' + value);
 }
